8a6e290cbcc5e6c2904096803c6d4e427a444b3a
vinhtrandev <se180339trananhvinh@gmail.com>
2026-04-12 05:07:50 +0700

    fix circular reference

--- tram/src/main/java/com/vinhtran/tram/security/JwtFilter.java
+++ tram/src/main/java/com/vinhtran/tram/security/JwtFilter.java
@@ -4,7 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,14 +13,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtFilter(JwtUtil jwtUtil, @Lazy UserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest req,
@@ -33,15 +38,12 @@ public class JwtFilter extends OncePerRequestFilter {
 
             if (jwtUtil.validate(token)) {
                 String nickname = jwtUtil.getNickname(token);
-
                 var userDetails = userDetailsService.loadUserByUsername(nickname);
-
                 var auth = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
                         userDetails.getAuthorities()
                 );
-
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
